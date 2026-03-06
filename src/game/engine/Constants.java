533b25b578c4e667f3f2722d39fba77a951adddc
Tasneem <elshafeytasneem@gmail.com>
2026-03-06 17:48:20 +0200

    Constants adding

--- src/game/engine/Constants.java
+++ src/game/engine/Constants.java
@@ -1,5 +1,20 @@
 package game.engine;
 
 public class Constants {
+	
+	//Board constants:
+	int BOARD_SIZE = 100;
+	int BOARD_ROWS = 10;
+	int BOARD_COLS = 10;
+	int WINNING_POSITION = 99;
+	int STARTING_POSITION = 0;
+	
+	//Special cells positions constants:
+	
+	int[] MONSTER_CELL_INDICES = {2, 18, 34, 54, 82, 88};
+	int[] CONVEYOR_CELL_INDICES = {6, 22, 44, 52, 66};
+	int[] SOCK_CELL_INDICES = {32, 42, 74, 84, 98};
+	int[] CARD_CELL_INDICES = {4, 12, 28, 36, 48, 56, 60, 76, 86, 90};
+	
 
 }
